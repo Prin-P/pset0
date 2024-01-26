eb68a8881448f16367197f0ca862de786fab5895
Prin-P <prinpulkes@gmail.com>
2024-01-25 21:22:23 -0500

    added comments

--- ps0/app.py
+++ ps0/app.py
@@ -6,37 +6,37 @@ app = Flask(__name__)
 # /// = relative path, //// = absolute path
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy(app) # Candidate concept: database
 
 
-class Todo(db.Model):
+class Todo(db.Model): #candidate concept: Model
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     complete = db.Column(db.Boolean)
 
 
-@app.route("/")
-def home():
+@app.route("/") #Candidate concept: Route
+def home(): #Candidate concept: Function
     todo_list = Todo.query.all()
     return render_template("base.html", todo_list=todo_list)
 
-@app.route("/add", methods=["POST"])
-def add():
+@app.route("/add", methods=["POST"]) #Candidate concept: Route
+def add(): #Candidate concept: Function
     title = request.form.get("title")
     new_todo = Todo(title=title, complete=False)
     db.session.add(new_todo)
     db.session.commit()
     return redirect(url_for("home"))
 
-@app.route("/update/<int:todo_id>")
-def update(todo_id):
+@app.route("/update/<int:todo_id>") #Candidate concept: Route
+def update(todo_id): #Candidate concept: Function
     todo = Todo.query.filter_by(id=todo_id).first()
     todo.complete = not todo.complete
     db.session.commit()
     return redirect(url_for("home"))
 
-@app.route("/delete/<int:todo_id>")
-def delete(todo_id):
+@app.route("/delete/<int:todo_id>") #Candidate concept: Route
+def delete(todo_id): #Candidate concept: Function
     todo = Todo.query.filter_by(id=todo_id).first()
     db.session.delete(todo)
     db.session.commit()
